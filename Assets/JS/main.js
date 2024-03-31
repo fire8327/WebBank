68a192994fa4f6c42118bcf57ff7c09d3b3d0ce7
fire8327 <114702513+fire8327@users.noreply.github.com>
2024-03-31 23:51:07 +0300

    create clocks

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -3,3 +3,13 @@ $("#menuToggler").click(() => {
     $("#menuList").toggleClass("max-lg:top-0 max-lg:-translate-y-full")
     $("#menuList").toggleClass("max-lg:top-full")
 })
+
+/* date */
+let date = new Date()
+setInterval(() => {
+    date = new Date()
+    $("#hours").html(`${date.getHours()}`)
+    $("#minutes").html(`${date.getMinutes()}`)
+    $("#seconds").html(`${date.getSeconds()}`)
+    $("#date").html(`${date.toLocaleDateString("ru" ,{ weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}`)
+}, 1000);
